852ebb8e6b3e65d4cd638f30d4e1bc3fae19c0be
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-10-04 22:02:31 +0200

    Add Stake model

---
+++ models/stake/functions/getStake.js
@@ -0,0 +1,19 @@
+module.exports = (project, callback) => {
+  if (!project || !project._id)
+    return callback('bad_request');
+
+  if (project.is_deleted)
+    return callback('not_authenticated_request');
+
+  return callback(null, {
+    _id: project._id.toString(),
+    is_active: project.is_active,
+    language: project.language,
+    name: project.name,
+    image: project.image,
+    stake_url: project.stake_url,
+    how_to_stake_url: project.how_to_stake_url,
+    apr: project.apr,
+    market_price: project.market_price
+  });
+}
